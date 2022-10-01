4f33b52741dbf88b26cb074f8593167c623bac1f
Mihai Alexandru Iordache <iordache.mihaialexandru@gmail.com>
2022-10-01 13:54:29 +0300

    Cleanup products component

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -1,20 +1,15 @@
 import React, { useEffect } from 'react'
-import { useSelector } from 'react-redux';
 import { clearProducts, getProducts } from '../../store/ducks/productsSlice';
 import { useAppDispatch } from '../../store/hooks';
-import { getProductsLength } from '../../store/selectors/products.selector';
 import Filters from './components/Filters';
 import ProductsList from './components/ProductsList';
 
 
 export const Products = () => {
   const dispatch = useAppDispatch();
-  const productsLength = useSelector(getProductsLength);
 
   useEffect(() => {
-    if (productsLength === 0) {
       dispatch(getProducts());
-    }
     return () => {
       dispatch(clearProducts());
     }
